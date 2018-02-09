cfb3507c6ab0bbade74a3c68ee8da5ce5968902b
Sean Oughton <seanoughton@gmail.com>
2018-02-09 17:28:59 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,13 +7,15 @@ class MP3Importer
       @path = path
     end
 
+    #files gets called by #import and returns an array
+    #files
     def files
-      @files ||= Dir.glob("#{@path}/*.mp3").collect{ |f| f.gsub("#{@path}/", "") }
+      @files = Dir.glob("#{@path}/*.mp3").collect{ |f| f.gsub("#{@path}/", "") }
     end
 
     # #import takes each normalized filename and sends it to the Song class #new_by_filename
       #which creates a new song instance for each of the filenames
-    # #import calls #files 
+    # #import calls #files
     #so it is calling the method on the MP3Importer instance
     def import
       self.files.each{|f| Song.new_by_filename(f)}
