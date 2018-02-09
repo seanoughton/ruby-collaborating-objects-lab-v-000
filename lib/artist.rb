fc76e47eb3816cc55ef5dc1ec72e0821f3d74b18
Sean Oughton <seanoughton@gmail.com>
2018-02-09 18:02:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,15 @@ class Artist
     @@all
   end
 
-  def add_song(song) #called by the song instance, adds in that song instance
+  def add_song(song)
+    #called by the song instance, adds in that song instance
+    #called inside the song method #artist_name=
+    #after the song has been created, assigned a name
+    #the song class checks to see if there is an artist associated with the song, and if there isnt' one, it asks the Artist class to create an artist instance
+    #it then passes that artist instance back to the song to store as an attribute
+    #the song class then calls this method #add_song
+    #this method adds the song to the artist instances song array
+    #every artist instance has this array
     @songs << song
   end
 
@@ -44,6 +52,7 @@ class Artist
     @@all << self
   end
 
+  #this method prints out an array of song names
   def print_songs()
     puts @songs.collect {|x| x.name}
   end
