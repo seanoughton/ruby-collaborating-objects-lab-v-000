dbf3457355a4cac976bd66237843ccd97e54def4
Sean Oughton <seanoughton@gmail.com>
2018-02-09 17:12:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,14 +18,14 @@ class Artist
     @songs << song
   end
 
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
+  def self.find_or_create_by_name(artist_name)
+    if !(@@all.find {|x|x.name == artist_name})
+      artist = self.new(artist_name) #creates a new artist instance
+      #@@all << artist #adds the artist instance to @all
+    else
+      (@@all.find {|x|x.name == artist_name})
+    end
   end
-
-  def self.find(name)
-    self.all.find {|artist| artist.name == name }
-  end
-
 
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
