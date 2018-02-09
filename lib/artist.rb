d2b158e1b4101a2175ac78cb57b1643e9aeda060
Sean Oughton <seanoughton@gmail.com>
2018-02-09 18:09:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,19 +26,23 @@ class Artist
     @songs << song
   end
 
+  #def self.find_or_create_by_name(name)
+  #end
+
+
+
+  #def self.create(name)
+  #end
+
   def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
   end
 
   def self.find(name)
-    @@all.find(name)
+    @@all.find do |x|
+      x.name == name
+    end
   end
-
-  def self.create(name)
-  end
-
-  #def self.find_or_create_by_name(name)
-    #self.find(name) ? self.find(name) : self.create(name)
-  #end
 
   #def self.find(name)
     #self.all.find {|artist| artist.name == name }
