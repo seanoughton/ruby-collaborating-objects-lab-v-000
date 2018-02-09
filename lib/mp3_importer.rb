fcdfc65d07300d6abe7f02b2d683f09ccfab221a
Sean Oughton <seanoughton@gmail.com>
2018-02-09 17:16:19 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,6 +11,9 @@ class MP3Importer
       @files ||= Dir.glob("#{@path}/*.mp3").collect{ |f| f.gsub("#{@path}/", "") }
     end
 
+
+    #this takes each normalized filename and sends it to the Song class method new_by_filename
+    #which creates a new song instance for each of the filenames
     def import
       files.each{|f| Song.new_by_filename(f)}
     end
