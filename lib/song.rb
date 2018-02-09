296f38498682051527c0802fa2589fbb51c8674f
Sean Oughton <seanoughton@gmail.com>
2018-02-09 17:47:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
 
-  attr_accessor :name, :artist
+  attr_accessor :name
+  attr_reader :artist
 
   def initialize(name)
     @name = name
@@ -10,10 +11,13 @@ class Song
     song_name = file.split(" - ")[1]
     artist = file.split(" - ")[0]
     song = self.new(song_name)
+    #this is calling the writer artist_name=
+    #it assigns that variable to artist
     song.artist_name = artist
     song
   end
 
+  #this is a setter method, a writer,
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
